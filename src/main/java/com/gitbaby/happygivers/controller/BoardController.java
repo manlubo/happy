faeceaedd7be575127e6a5260b28a347f8c62133
manlubo <manlubo11@gmail.com>
2025-07-19 16:55:57 +0900

    게시글 내부 댓글 구현 완료(비동기 처리)

--- src/main/java/com/gitbaby/happygivers/controller/BoardController.java
+++ src/main/java/com/gitbaby/happygivers/controller/BoardController.java
@@ -9,6 +9,7 @@ import com.gitbaby.happygivers.domain.en.Status;
 import com.gitbaby.happygivers.service.BoardService;
 import com.gitbaby.happygivers.service.DonateService;
 import com.gitbaby.happygivers.service.MemberService;
+import com.gitbaby.happygivers.service.ReplyService;
 import com.gitbaby.happygivers.util.AlertUtil;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
@@ -16,22 +17,48 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.beans.PropertyEditorSupport;
 import java.io.IOException;
 import java.lang.reflect.Type;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @AllArgsConstructor
 @Slf4j
 public class BoardController {
+  private ReplyService replyService;
   private BoardService boardService;
   private DonateService donateService;
   private MemberService memberService;
+
+
+  // 문자열 null을 null 처리하는 binder
+  @InitBinder
+  public void initBinder(WebDataBinder binder) {
+    binder.registerCustomEditor(Integer.class, new PropertyEditorSupport() {
+      @Override
+      public void setAsText(String text) {
+        if (text == null || text.equals("") || text.equalsIgnoreCase("null")) {
+          setValue(null);
+        } else {
+          setValue(Integer.parseInt(text));
+        }
+      }
+    });
+  }
+
+
+
+
+
+
   // 리스트
   @GetMapping({"board/list", "notice/list", "qna/list"})
   public String boardList(Criteria cri, Model model, HttpServletRequest req) {
@@ -95,6 +122,7 @@ public class BoardController {
     return "board/view";
   }
 
+  // 게시글 작성 폼
   @GetMapping({"board/write", "notice/write", "qna/write"})
   public String boardWriteForm(@SessionAttribute(value = "member", required = false) Member member, Model model, Criteria cri, HttpServletRequest req) {
     if(member == null || !member.getStatus().equals(Status.ACTIVE)) {
@@ -224,6 +252,120 @@ public class BoardController {
 
     return AlertUtil.alert("글이 수정되었습니다.", "list", model);
   }
+
+
+  // 피드 리스트
+  @GetMapping("feed/list")
+  public String feedListForm(Criteria cri, Model model){
+    if(cri == null) {
+      cri = new Criteria();
+    }
+    cri.setStatus(Status.ACTIVE);
+    cri.setCtype(Ctype.FEED);
+    List<Board> feeds = boardService.list(cri);
+
+    model.addAttribute("pageDto", new PageDto(cri, boardService.getCount(cri)));
+    model.addAttribute("feeds", feeds);
+
+    return "board/feed/list";
+  }
+
+
+  // 피드 비동기 처리
+  @ResponseBody
+  @GetMapping("api/feed/view")
+  public Map<String, Object> feedViewForm(@RequestParam("bno") Long bno, @SessionAttribute(value = "member", required = false) Member member, Model model){
+    Board board = boardService.findByBno(bno);
+    Long mno = (member == null) ? null : member.getMno();
+    if(board == null) {
+      AlertUtil.alert("해당 글을 찾을 수 없습니다.", "list", model);
+    }
+
+    List<Reply> replys = replyService.list(bno, mno, null);
+    String profile = memberService.findByMno(board.getMno()).getProfile();
+    board.setLiked(boardService.checkBoardLiked(board.getBno(), mno));
+
+    Map<String, Object> data = Map.of(
+      "board", board,
+      "replys", replys,
+      "profile", profile
+    );
+
+    return data;
+  }
+
+
+
+  // 피드 게시글 작성
+  @GetMapping("feed/write")
+  public String feedWriteForm(@SessionAttribute(value = "member", required = false) Member member, Model model){
+    if(member == null ||  !member.getStatus().equals(Status.ACTIVE)) {
+      return AlertUtil.alert("회원가입이 완료된 회원만 글을 작성하실 수 있습니다.", "list", model);
+    }
+    return "board/feed/write";
+  }
+
+  @PostMapping("feed/write")
+  public String feedWrite(@SessionAttribute(value = "member", required = false) Member member, Board board, Attach attach, Model model){
+    if(member == null ||  !member.getStatus().equals(Status.ACTIVE)) {
+      return AlertUtil.alert("회원가입이 완료된 회원만 글을 작성하실 수 있습니다.", "list", model);
+    }
+    if(attach.getUuid() != null) {
+      attach.setMno(null);
+      board.setAttach(attach);
+    }
+
+    boardService.write(board);
+    return AlertUtil.alert("글이 등록되었습니다.", "list", model);
+  }
+
+  // 피드 수정
+  @GetMapping("feed/modify")
+  public String feedModifyForm(@SessionAttribute(value = "member", required = false) Member member, Model model, @RequestParam("bno") Long bno){
+    if(member == null ||  !member.getStatus().equals(Status.ACTIVE) || !(member.getMno() == boardService.findByBno(bno).getMno() || member.getMtype().equals(Mtype.ADMIN))) {
+      return AlertUtil.alert("자신의 글만 수정이 가능합니다.", "list", model);
+    }
+
+    model.addAttribute("feed", boardService.findByBno(bno));
+    return "board/feed/modify";
+  }
+
+  @PostMapping("feed/modify")
+  public String feedModify(Board board, Attach attach, Model model){
+    if(attach.getUuid() != null) {
+      attach.setMno(null);
+      board.setAttach(attach);
+    }
+    boardService.modify(board);
+    return AlertUtil.alert("수정이 완료되었습니다.", "list", model);
+  }
+
+  // 공지사항 Q&A 상세
+  @GetMapping({"notice/view", "qna/view"})
+  public String normalViewForm(@RequestParam("bno") Long bno, Model model, HttpServletRequest req, Criteria cri) {
+    if(bno == null) {
+      return AlertUtil.alert("잘못된 접근입니다.", "list", model);
+    }
+    String path = req.getServletPath();
+    log.info("{}", path);
+
+    if(cri == null) {
+      cri = new Criteria();
+    }
+
+    if ("/notice/view".equals(path)) {
+      cri.setCtype(Ctype.NOTICE);
+    }
+    else if ("/qna/view".equals(path)) {
+      cri.setCtype(Ctype.QNA);
+    }
+
+    model.addAttribute("board", boardService.findByBno(bno));
+    model.addAttribute("cri", cri);
+    return "board/other/view";
+  }
+
+
 
 
 }
---
+++ src/main/java/com/gitbaby/happygivers/controller/ReplyController.java
@@ -0,0 +1,54 @@
+package com.gitbaby.happygivers.controller;
+
+import com.gitbaby.happygivers.domain.Member;
+import com.gitbaby.happygivers.domain.Reply;
+import com.gitbaby.happygivers.service.BoardService;
+import com.gitbaby.happygivers.service.ReplyService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@AllArgsConstructor
+@Slf4j
+@RestController
+@RequestMapping("reply")
+public class ReplyController {
+  private ReplyService replyService;
+
+  @GetMapping("{rno}")
+  public Reply get(@PathVariable Long rno){
+    return replyService.findBy(rno);
+  }
+
+  @GetMapping({"list/{bno}", "list/{bno}/{lastRno}"})
+  public List<Reply> list(@PathVariable Long bno, @PathVariable(required = false) Long lastRno, @SessionAttribute(value = "member", required = false ) Member member){
+    Long mno = member == null ? 0L : member.getMno();
+    return replyService.list(bno, mno, lastRno);
+  }
+
+  @PostMapping("/")
+  public Map<String,Object> write(@RequestBody Reply reply){
+    replyService.register(reply);
+    return Map.of("result", true, "reply", reply);
+  }
+
+  @PutMapping("{rno}")
+  public Map<String,Object> write(@RequestBody Reply reply, @PathVariable Long rno){
+    replyService.modify(reply);
+    return Map.of("result", true, "reply", reply);
+  }
+
+  @DeleteMapping("{rno}")
+  public ResponseEntity<Map<String,Object>> delete(@PathVariable Long rno){
+    replyService.remove(rno);
+    return ResponseEntity.ok().body(Map.of("result", true));
+  }
+
+}
